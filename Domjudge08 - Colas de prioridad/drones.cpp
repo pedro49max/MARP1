f070b003721b1f22dfa48dc45cb79a9a9cbfd2b2
pedro49max <pedrotob@ucm.es>
2025-10-18 11:10:44 +0200

    08

--- Domjudge08 - Colas de prioridad/drones.cpp
+++ Domjudge08 - Colas de prioridad/drones.cpp
@@ -22,29 +22,29 @@ bool solve(int N, int A,int  B) {
 		pilas15v.push(v);
 	}
 	bool first = true;
-	while (!pilas9v.empty() && !pilas15v.empty()) {
+	while (!pilas9v.empty() && !pilas15v.empty()) {//O(min(A,B) log A + min(A,B) log B)
 		int horas = 0;
 		pq pilas9vReutilizar;
 		pq pilas15vReutilizar;
-		for (int i = 0; i < N && (!pilas9v.empty() && !pilas15v.empty()); i++) {
-			int vatios9 = pilas9v.top(); pilas9v.pop();
-			int vatios15 = pilas15v.top(); pilas15v.pop();
+		for (int i = 0; i < N && (!pilas9v.empty() && !pilas15v.empty()); i++) {//O(N log A + N log B)
+			int vatios9 = pilas9v.top(); pilas9v.pop();//O(log A)
+			int vatios15 = pilas15v.top(); pilas15v.pop();//O(log A)
 			if (vatios9 > vatios15) {
 				horas += vatios15;
-				pilas9vReutilizar.push(vatios9 - vatios15);
+				pilas9vReutilizar.push(vatios9 - vatios15);//O(log B)
 			}
 			else if (vatios9 < vatios15) {
 				horas += vatios9;
-				pilas15vReutilizar.push(vatios15 - vatios9);
+				pilas15vReutilizar.push(vatios15 - vatios9);//O(log B)
 			}
 			else
 				horas += vatios9;
 		}
-		while (!pilas15vReutilizar.empty()) {
-			pilas15v.push(pilas15vReutilizar.top()); pilas15vReutilizar.pop();
+		while (!pilas15vReutilizar.empty()) {//O(log A)
+			pilas15v.push(pilas15vReutilizar.top()); pilas15vReutilizar.pop();//O(log B)
 		}
-		while (!pilas9vReutilizar.empty()) {
-			pilas9v.push(pilas9vReutilizar.top()); pilas9vReutilizar.pop();
+		while (!pilas9vReutilizar.empty()) {//O(log A)
+			pilas9v.push(pilas9vReutilizar.top()); pilas9vReutilizar.pop();//O(log B)
 		}
 		if (!first)
 			cout << " ";
