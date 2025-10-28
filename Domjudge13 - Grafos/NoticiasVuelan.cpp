57225b5ef9a9eeb8e0ad7576fa3ad72b3df832ae
pedro49max <pedrotob@ucm.es>
2025-10-28 18:21:45 +0100

    13

--- Domjudge13 - Grafos/NoticiasVuelan.cpp
+++ Domjudge13 - Grafos/NoticiasVuelan.cpp
@@ -2,63 +2,52 @@
 #include<fstream>
 #include <vector>
 #include<queue>
-#include "Grafo.h"
+#include"ConjuntosDisjuntos.h"
 using namespace std;
 
-void dfs(const Grafo& grafo, int v, vector<bool>& visited, int& count, vector<bool> &group) {
-    queue<int> q;
-    visited[v] = true;
-    group[v] = true;
-    count++;
-    for (int w : grafo.ady(v)) {
-        if (!visited[w]) {
-            dfs(grafo, w, visited, count, group);
+
+
+
+void solve() {
+    int N, M;
+    while (cin >> N >> M) {
+        ConjuntosDisjuntos conjuntos(N);
+        for (int i = 0; i < M; i++) {
+            int f;
+            cin >> f;
+            int a;
+            if (f > 0)
+                cin >> a;
+            for (int j = 1; j < f; j++) {
+                int b;
+                cin >> b;
+                conjuntos.unir(a - 1, b - 1);
+            }
         }
+        if (N > 0)
+            cout << conjuntos.tam(0);
+        for (int i = 1; i < N; i++) {
+            cout << " " << conjuntos.tam(i);
+        }
+        cout << endl;
     }
 }
 
 int main() {
+        // Para la entrada por fichero.
+            // Comentar para acepta el reto
 #ifndef DOMJUDGE
     std::ifstream in("in.txt");
     auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
+
+    solve();
+
+
+    // Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+    std::cin.rdbuf(cinbuf);
+    system("PAUSE");
 #endif
-    int V,M;
-    
-    while(cin >> V >> M) { //O(V +A)
-        Grafo grafo(V);
-        for (int i = 0; i < M; ++i) {//O(A*M)
-            int A;
-            cin >> A;
-            vector<int> amigos(A);
-            for (int x = 0; x < A; x++) {
-                int aux;
-                cin >> aux;
-                amigos[x]= aux - 1;
-            }
-            for (int a1 = 1; a1 < A; a1++) {
-                grafo.ponArista(amigos[0], amigos[a1]);
-            }
-        }
-        vector<int> component_size(V, 0);
-        vector<bool> visited(V, false);
-        for (int v = 0; v < V; ++v) {
-            if (!visited[v]) {
-                vector<bool> group(V, false);
-                int count = 0;
-                dfs(grafo, v, visited, count, group);
-                // Asignamos el tamaño de la componente a todos sus miembros
-                for (int u = 0; u < V; ++u) {
-                    if (group[u]) {
-                        component_size[u] = count;
-                    }
-                }
-            }
-        }
-        cout << component_size[0]; //O(V+A)
-        for (int i = 1; i < V; i++) {
-            cout << " " << component_size[i];
-        }
-        cout << "\n";
-    }
     return 0;
 }
